7eb8e9084749c2108512ffd2eade4c16a5fcbc08
wen.5.zhang <153814959@qq.com>
2019-04-03 20:49:55 +0800

    add ajax

---
+++ cgi-bin/buy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import time
+
+time.sleep(5)
+print "Content-type:text/html"
+print
+print "<html>"
+print "<body>"
+car = 'A Car'
+print "<h1> I want to buy %s </h1>"%car
+print "</body>"
+print "</html>"
